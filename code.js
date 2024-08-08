652e6fc12b0590a6a5674fe1311de44dbff75398
jaydenater1234 <79484210+jaydenater1234@users.noreply.github.com>
2024-08-08 12:09:57 -0500

    code

---
+++ code.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('clickMeButton');
+
+    var message = document.getElementById('message');
+
+
+    button.addEventListener('click', function() {
+        message.textContent = 'im jayden im learning how to code ';
+    });
+});
+
